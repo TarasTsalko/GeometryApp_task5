07d1e4306264709a6b83c981099283a7ac339be6
Taras Tsalko <you@example.com>
2025-07-26 15:04:27 +0300

    Правки по ревью: метод Top возвращает константную ссылку на точку, вместо копии

--- src/convex_hull.cpp
+++ src/convex_hull.cpp
@@ -15,7 +15,7 @@ class StackForGrahamScan {
 public:
     void Pop() { hull.pop(); }
 
-    [[nodiscard]] Point2D Top() const { return hull.top(); }
+    [[nodiscard]] const Point2D &Top() const { return hull.top(); }
 
     void Push(const Point2D &point) { hull.push(point); }
 
